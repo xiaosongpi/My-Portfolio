2f77eba0e04dfd85823b87fb486e9634ec41da24
xiaosongpi <minixiaopipi@gmail.com>
2025-10-10 11:05:22 +0700

    Modify navigation bar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -3,29 +3,15 @@ import styles from './Navbar.module.css'
 function Navbar () {
     return (
         <nav className={styles.nav}>
-            <div className={styles.nav__logo}>
-                <div className=''></div>
-                <p className={styles.nav__brand}>Portfolio</p>
-            </div>
-            <ul className={styles.menu__list}>
-                <li>
-                    <a href="">About</a>
-                </li>
-                <li>
-                    <a href="">Skill</a>
-                </li>
-                <li>
-                    <a href="">Project</a>
-                </li>
-                <li>
-                    <a href="">Contact</a>
-                </li>
-            </ul>
-            <div className={styles.nav__action}>
-                <div className={styles.user}>
-                    <p className={styles.user__name}>Zhomphilies</p>
-                    <div className={`${styles.user__status} ${styles['user__status--small']}`}></div>
-                </div>
+            <p className={styles.title}>portfolio</p>
+            <div className={styles['box--right']}>
+                <ul className={styles.list}>
+                    <li className={styles.item}>about/</li>
+                    <li className={styles.item}>skill/</li>
+                    <li className={styles.item}>project/</li>
+                    <li className={styles.item}>contact/</li>
+                    <div className={styles.circle}></div>
+                </ul>
             </div>
         </nav>
     )
